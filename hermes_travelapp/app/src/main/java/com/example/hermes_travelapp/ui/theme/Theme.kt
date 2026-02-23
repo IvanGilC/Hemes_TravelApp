99b7c846c069cbfa9e957a284ec686a568821126
IvanGilC <ivangil2000@gmail.com>
2026-02-23 09:14:14 +0100

    v0.0.3 - Eliminacion y recreacion del app en el lugar correcto

--- hermes_travelapp/app/src/main/java/com/example/hermes_travelapp/ui/theme/Theme.kt
+++ hermes_travelapp/app/src/main/java/com/example/hermes_travelapp/ui/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun Hermes_TravelAppTheme(
+fun Hermes_travelappTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
