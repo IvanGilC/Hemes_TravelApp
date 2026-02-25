80023fcad9d61bb99fcfbf699d17ba0bcf5d6964
LAPTOP-SE8LD8AL\E1404 <marcoberuetmorelli@gmail.com>
2026-02-25 12:05:27 +0100

    Splash Screen

--- hermes_travelapp/app/src/main/java/com/example/hermes_travelapp/NavGraph.kt
+++ hermes_travelapp/app/src/main/java/com/example/hermes_travelapp/NavGraph.kt
@@ -7,11 +7,13 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.hermes_travelapp.screens.HomeScreen
 import com.example.hermes_travelapp.screens.ProfileScreen
+import com.example.hermes_travelapp.screens.SplashScreen
 
 @Composable
 fun NavGraph(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "home", modifier = modifier) {
+    NavHost(navController = navController, startDestination = "splash", modifier = modifier) {
+        composable("splash") { SplashScreen(navController = navController) }
         composable("home") { HomeScreen(navController = navController) }
         composable("profile") { ProfileScreen() }
     }
